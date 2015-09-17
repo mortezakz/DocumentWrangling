66a4c8b63e26594d0152deef4d295066d4aad11f
Connor Henderson <cvh5268@psu.edu>
2015-09-17 12:06:40 -0400

    Started work on accidental deletion issue with Authors' names.  Added a few lines of code so that first row of headings is printed but not cleaned.

--- src/csvhandling/TestClass.java
+++ src/csvhandling/TestClass.java
@@ -11,41 +11,7 @@ import au.com.bytecode.opencsv.CSVWriter;
 public class TestClass {
 
 	/*public void writeTest() throws IOException {
-		CSVReader reader = new CSVReader(new FileReader("NSF_master_table.csv"));
-		CSVWriter writer = new CSVWriter(new FileWriter("ModifiedTable.csv"), ',');
-
-		String[] nextLine;
-
-		String[] newFileLine;
-
-		int count = 0;
-
-		while ((nextLine = reader.readNext()) != null) {
-			// nextLine[] is an array of values from the line
-			System.out.println(nextLine);
-			newFileLine = nextLine;
-
-			// newFileLine[19] = nextLine[19].toUpperCase();
-
-			// THIS IS WHER THE PROBLEM WAS. I WAS GETTING nextLine[2] instead
-			// of [3]. It appears that the award number actually shows up in
-			// different place in text. Not just the beginning.
-			String awardNumber = nextLine[3];
-
-			if (nextLine[19].indexOf(awardNumber) != -1) {
-				System.out.println(++count);
-				System.out.println(nextLine[19].indexOf(awardNumber));
-
-				newFileLine[19] = nextLine[19].substring(nextLine[19].indexOf(awardNumber) + awardNumber.length() , newFileLine[19].length());
-
-			}
-
-			writer.writeNext(newFileLine);
-
-			System.out.println(nextLine[19] + nextLine[1] + "etc...");
-		}
-
-		writer.close();
+		
 	}*/
 	
 	public static void main(String[] args) throws IOException {
@@ -62,7 +28,10 @@ public class TestClass {
 
 		int count = 0;
 
-		nextLine = reader.readNext(); //skips first row, which contains only headings
+		//prints first row (column headings) without cleaning
+		nextLine = reader.readNext();
+		newFileLine = nextLine;
+		writer.writeNext(newFileLine);
 		
 		while ((nextLine = reader.readNext()) != null) {
 			// nextLine[] is an array of values from the line
@@ -89,7 +58,8 @@ public class TestClass {
 			// checks for and deletes author name (lower case)
 			String authorName = nextLine[24];
 		
-			if (nextLine[19].indexOf(authorName) != -1) {
+			if ((nextLine[19].indexOf(authorName) != -1) &&
+					(nextLine[19].substring(nextLine[19].indexOf(authorName) + authorName.length()).startsWith("[A-Z]"))) {
 				System.out.println(++count);
 				System.out.println(nextLine[19].indexOf(authorName));
 
@@ -112,19 +82,20 @@ public class TestClass {
 			
 			// ISSUE: unintended name deletion at Row 43, others
 			
-			// checks for and deletes institution (lower case)
+			// checks for and deletes institution name (lower case)
 			String institutionName = nextLine[8];
 					
-			if (nextLine[19].indexOf(institutionName) != -1) {
+			if ((nextLine[19].indexOf(institutionName) != -1) &&
+					(nextLine[19].substring(nextLine[19].indexOf(institutionName) + institutionName.length()).startsWith("* [A-Z]"))) {
 				System.out.println(++count);
 				System.out.println(nextLine[19].indexOf(institutionName));
 
-				nextLine[19] = newFileLine[19];			
+				nextLine[19] = newFileLine[19];
 				newFileLine[19] = nextLine[19].substring(nextLine[19].indexOf(institutionName) + institutionName.length() , newFileLine[19].length());
 
 			}
 			
-			// checks for and deletes institution (upper case)
+			// checks for and deletes institution name (upper case)
 			institutionName = nextLine[8].toUpperCase();
 								
 			if (nextLine[19].indexOf(institutionName) != -1) {
